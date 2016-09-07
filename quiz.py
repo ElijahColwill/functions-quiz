d0be0dfe039265e58f5568825c2750e852463117
Elijah Robert Colwill <colwiller@dcsdk12.org>
2016-09-07 12:08:24 -0600

    Implement has_teen

--- quiz.py
+++ quiz.py
@@ -1,5 +1,11 @@
-	
-# TODO - write has_teen
+
+def has_teen(a, b, c):
+	return (a >= 13 and a <= 19) or (b >= 13 and b <= 19) or (c >= 13 and c <= 19)
+
+print has_teen(11, 14, 18)
+print has_teen(12, 2, 19)
+print has_teen(12, 2, 11)
+
 
 # TODO - write not_string
 
