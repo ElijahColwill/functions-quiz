79ed3253d26e864b899ab85ec10cd72137d48dbd
Elijah Robert Colwill <colwiller@dcsdk12.org>
2016-09-07 12:13:15 -0600

    Implement not_string

--- quiz.py
+++ quiz.py
@@ -6,8 +6,15 @@ print has_teen(11, 14, 18)
 print has_teen(12, 2, 19)
 print has_teen(12, 2, 11)
 
+def not_string(str):
+	if str.startswith("Not") or str.startswith("not"):
+		return str + "Not"
+	else: return "Not" + str
 
-# TODO - write not_string
+print not_string(" welcome here.")
+print not_string("Not welcome here? ")
+print not_string("Not cool? ")
+print not_string("Cool. ")
 
 # TODO - write icy_hot
 
